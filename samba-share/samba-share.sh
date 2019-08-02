756a36ddc2d0a69757ecd18917ca1a2cc8e51fdb
Aldo D'Aquino <daquinoaldo@users.noreply.github.com>
2019-08-02 13:09:10 +0200

    Update samba-share.sh

--- samba-share/samba-share.sh
+++ samba-share/samba-share.sh
@@ -2,3 +2,6 @@ sudo apt update
 sudo apt install -y samba samba-common-bin
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.BAK
 sudo /bin/sh -c 'curl https://raw.githubusercontent.com/daquinoaldo/RaspberryPi/master/samba-share/samba.conf >> /etc/samba/smb.conf'
+echo "Set a password for samba:"	
+sudo smbpasswd -a pi	
+sudo /etc/init.d/samba restart
