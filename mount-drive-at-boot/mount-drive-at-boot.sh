cfeadf3fff1c1054c9190f63b328e233a24a1706
Aldo D'Aquino <daquinoaldo@users.noreply.github.com>
2019-08-02 12:51:31 +0200

    Update mount-drive-at-boot.sh

--- mount-drive-at-boot/mount-drive-at-boot.sh
+++ mount-drive-at-boot/mount-drive-at-boot.sh
@@ -3,6 +3,3 @@ sudo apt install -y ntfs-3g
 sudo mkdir /mnt/usb
 sudo chmod 1777 /mnt/usb
 sudo /bin/sh -c 'echo "/dev/sda1 /mnt/usb ntfs-3g defaults 0 0" >> /etc/fstab'
-echo "Set a password for samba:"
-sudo smbpasswd -a pi
-sudo /etc/init.d/samba restart
